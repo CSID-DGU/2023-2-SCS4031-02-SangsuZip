9319090b9ebc217e8d1895b15f4e21e81e49a5a0
kwan2 <jgh9987@naver.com>
2023-11-16 08:27:50 +0900

    test: aws cli 타 로컬에서도 가능한지

--- serverless/app.py
+++ serverless/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, make_response
+from transformers import pipeline
 
 app = Flask(__name__)
 
+generator = pipeline('text-generation', model='gpt3.5')
 
 @app.route("/")
 def hello_from_root():
@@ -12,7 +14,16 @@ def hello_from_root():
 def hello():
     return jsonify(message='Hello from path!')
 
+@app.route('/generate_text', methods=['POST'])
+def generate_text():
+    data = request.get_json()
+    prompt = data['prompt']
+    generated_text = generator(prompt, max_length=100)[0]['generated_text']
+    
+    response = {'generated_text': generated_text}
+    return jsonify(response)
 
 @app.errorhandler(404)
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
+
