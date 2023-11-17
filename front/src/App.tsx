5acdb98aac65bc8d315fb502c8962f652fdaf34f
wonho1401 <wonho1401@naver.com>
2023-11-17 11:20:40 +0900

    :recycle: Refactor: use routes and route instead of routerprovider
    refactor: use routes and route instead of routerprovider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,24 @@
 import { GlobalStyle } from "./styles/GlobalStyle.tsx";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import theme from "./styles/theme.ts";
-import { RouterProvider } from "react-router";
-import { router } from "./routes/router.tsx";
 import Layout from "./components/layout/Layout.tsx";
+import Home from "./pages/Home/Home.tsx";
+import Write from "./pages/Write/Write.tsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <RouterProvider router={router} />
-      </Layout>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/write" element={<Write />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
