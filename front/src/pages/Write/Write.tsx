93ad40d82e17830672b4df3d947b42315820f16f
wonho1401 <wonho1401@naver.com>
2023-11-23 22:49:18 +0900

    :sparkles: Feat: add write feeds page
    feat: add write feeds page

---
+++ front/src/pages/Write/Write.tsx
@@ -0,0 +1,120 @@
+import { useCallback, useState } from "react";
+import MDEditor from "@uiw/react-md-editor";
+import * as S from "./style";
+import Button from "../../components/common/button/Button";
+import theme from "../../styles/theme";
+
+function Write() {
+  const [value, setValue] = useState<string | undefined>();
+  const [hashtag, setHashtag] = useState<string | "">("");
+  const [hashArr, setHashArr] = useState<string[] | []>([]);
+  const [title, setTitle] = useState<string | "">("");
+
+  const onChangeHashTag = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHashtag(e.target.value);
+  };
+
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const onKeyUpHandler = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      const $hashArr = document.querySelector(".hashArr");
+      const $hashTags = document.createElement("div");
+      $hashTags.className = "hashTags";
+      $hashTags.style.color = theme.color.point1;
+      $hashTags.style.border = `1px solid ${theme.color.gray40}`;
+      $hashTags.style.borderRadius = "0.75rem";
+      $hashTags.style.backgroundColor = "white";
+      $hashTags.style.padding = "0.5rem 1rem";
+      $hashTags.style.cursor = "pointer";
+
+      const value = (e.target as HTMLInputElement).value;
+
+      $hashTags.addEventListener("click", () => {
+        $hashArr?.removeChild($hashTags);
+        setHashArr(hashArr.filter((hash) => hash));
+      });
+      /**
+       * 태그 입력 값이 있고, Enter를 눌렀을 때 태그 추가.
+       */
+      if (e.key === "Enter" && value.trim() !== "") {
+        $hashTags.innerHTML = `<span>#${value}</span>`;
+        $hashArr?.appendChild($hashTags);
+        setHashArr((prev) => [...prev, value]);
+        setHashtag("");
+      }
+      /**
+       * 태그 입력 값이 없고, Backspace를 눌렀을 때 마지막 태그 삭제.
+       */
+      if (
+        e.key === "Backspace" &&
+        hashtag.length === 0 &&
+        hashArr.length !== 0
+      ) {
+        $hashArr?.lastElementChild?.remove();
+        setHashArr(hashArr.slice(0, -1));
+      }
+    },
+    [hashArr, hashtag]
+  );
+
+  return (
+    <S.Container>
+      <S.TopContainer>
+        <S.TitleContainer>
+          <S.Title
+            placeholder="제목을 입력해주세요."
+            type="text"
+            value={title}
+            onChange={onChangeTitle}
+          />
+          <S.TagContainer>
+            <S.EnteredTag className="hashArr"></S.EnteredTag>
+            <S.TagInput
+              placeholder="태그를 입력해주세요."
+              type="text"
+              value={hashtag}
+              onChange={onChangeHashTag}
+              onKeyUp={onKeyUpHandler}
+            />
+          </S.TagContainer>
+        </S.TitleContainer>
+        <S.ButtonContainer>
+          <Button
+            $width={5}
+            $height={2}
+            text="임시저장"
+            $hasBorder={true}
+            $backgroundColor={theme.color.gray40}
+            $borderColor={theme.color.gray40}
+            $color={theme.color.gray80}
+            onClick={() => {}}
+          />
+          <Button
+            $width={5}
+            $height={2}
+            text="업로드"
+            $hasBorder={true}
+            $backgroundColor={theme.color.point1}
+            $borderColor={theme.color.point1}
+            $color="white"
+            onClick={() => {}}
+          />
+        </S.ButtonContainer>
+      </S.TopContainer>
+      <S.EditorContainer>
+        <MDEditor
+          data-color-mode="light"
+          height={"100vh"}
+          value={value}
+          placeholder="내용을 입력해주세요!"
+          onChange={setValue}
+        />
+      </S.EditorContainer>
+    </S.Container>
+  );
+}
+
+export default Write;
---
+++ front/src/pages/Write/style.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+const { color } = theme;
+
+export const Container = styled.div`
+  width: 100vw;
+  background-color: ${color.gray20};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TopContainer = styled.div`
+  margin-top: 6rem;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Title = styled.input`
+  width: 40vw;
+  height: 3rem;
+  padding: 0.5rem;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid ${color.gray40};
+  border-radius: 5px;
+  background-color: transparent;
+  ${theme.fonts.t16Medium};
+`;
+
+export const TagContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const EnteredTag = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const TagInput = styled.input`
+  width: 20vw;
+  height: 3rem;
+  padding: 0.5rem;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid ${color.gray40};
+  border-radius: 5px;
+  background-color: transparent;
+  ${theme.fonts.t14Medium};
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const EditorContainer = styled.div`
+  width: 100vw;
+  padding: 0 2rem;
+`;
