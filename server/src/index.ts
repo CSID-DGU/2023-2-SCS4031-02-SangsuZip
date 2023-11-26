7361331a6f012379b0236a586eb5a273d3ec1a47
pgrgrgrgr <parkracoon@naver.com>
2023-11-26 17:41:36 +0900

    Feat: user signup

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = 4000;
 
 // mongodb 연결
-const mongoURI = 'mongodb-connection-string';
+const mongoURI = 'mongodb+srv://parkracoon:pt1221@kangho.k4mc7hv.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoURI);
 
 const db = mongoose.connection;
@@ -16,6 +16,8 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+
+// middleware
 // cors 설정
 const corsOptions = {
   origin: "http://localhost:3000",
@@ -23,6 +25,8 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+// body-parser
+app.use(express.json())
 
 // routes
 app.use('/', Api)
--- server/src/routes/Api.ts
+++ server/src/routes/Api.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-import { write } from '../controllers/Api/feed';
+import { write } from '../controllers/Api/Feed'
+import { signup } from '../controllers/Api/User'
 
 const router = express.Router();
+
+router.post('/signup', signup)
 
 router.post('/write', write);
 
