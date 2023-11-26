352839667a3bd4a0a798e06388c08bd8c0871272
pgrgrgrgr <parkracoon@naver.com>
2023-11-26 16:44:31 +0900

    Feat: cors 설정

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 const port = 4000;
@@ -13,6 +14,14 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
+// cors 설정
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
