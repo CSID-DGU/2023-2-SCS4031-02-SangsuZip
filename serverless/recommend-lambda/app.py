e122229aa02ef7cb9f88b7d958550a879730ebf6
Ubuntu <ubuntu@ip-172-31-3-239.ap-northeast-2.compute.internal>
2023-11-27 10:31:33 +0000

    Fix: gpt prompt scripting, 답변 정확성 개선

--- serverless/recommend-lambda/app.py
+++ serverless/recommend-lambda/app.py
@@ -1,8 +1,60 @@
-from flask import Flask
-import openai
+from flask import Flask, request
+from openai import OpenAI
+import json
+import os
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route("/")  
-def hello():
-    return "Hello, World!"
+client = OpenAI(api_key = os.getenv("apikey"))
+model = "gpt-3.5-turbo"
+
+def generate_openai_response(question):
+    response = client.chat.completions.create(
+        model = model,
+        messages = [
+            {
+                "role": "system",
+                "content": "You can only answer in json format like {technique one name as key : short description about the technique as value, technique two name as key : ...}"
+            },
+            {
+                "role": "system",
+                "content": "When answering, please state the key value in English and the value value in Korean."
+            },
+            {
+                "role": "system",
+                "content": "Please focus on technologies that the author did not mention."
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
+
+@app.route("/", methods = ['POST'])
+def openai_response():
+    try:
+        req = request.get_json()
+        print(req)
+        content = req['content']
+
+        message = "Recommend three development-related learning techniques that this developer would like to study next. The following is what the developer posted on his blog about the study material he learned today. This developer organized the main content is as follows: " + content + ""
+
+        response = generate_openai_response(message)
+
+        response_body = {
+            'message': response
+        }
+
+        return {
+            'question': message,
+            'statusCode': 200,
+            'body': json.dumps(response_body)
+        }
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'args': request, 'error': str(e)})
+        }
