875d819e1c0be5f77ebd6ff1271f4294505e9b5a
kwan2 <jgh9987@naver.com>
2023-11-27 17:04:43 +0900

    fix: GPT api 추천 데이터 저장 구현

--- server/src/services/LambdaService.ts
+++ server/src/services/LambdaService.ts
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 import axios from 'axios';
 import RecommendDTO from '../models/RecommendDTO';
+import Feed from '../models/Feed';
 
 dotenv.config();
 
 export class LambdaService {
-    async invokeLambda (content : string ) : Promise<any> {
+    async invokeLambda (content : string, _id : string ) : Promise<any> {
         const lambdaURL : string = process.env.AWS_LAMBDA_URL || '';
         try{
             const reqbody = {
@@ -19,6 +20,21 @@ export class LambdaService {
             
             const resbody = JSON.parse(tmpJsonObj.message)
             
+            const recommendedTags = Object.keys(resbody);
+            
+
+            await Feed.findById(_id)
+                .exec()
+                .then(beforeFeed => {
+                    if(beforeFeed){
+                        beforeFeed.recommendedTags = beforeFeed.recommendedTags.concat(recommendedTags);
+                        beforeFeed.save();
+                    }
+                })
+                .catch(err => {
+                    console.log('Feed 저장 에러 발생 : ' + err);
+                })
+            
             const dto : RecommendDTO[] = [];
             
             for( const tag in resbody){
@@ -26,8 +42,8 @@ export class LambdaService {
                     dto.push({ tag: tag, description : resbody[tag] });
                 }
             }
-
             return dto;
+
         } catch (err) {
             console.error(err);
         }
