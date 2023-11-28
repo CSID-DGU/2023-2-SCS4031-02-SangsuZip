b7c8efa59013a351282ee132a1577a09488c3b94
wonho1401 <wonho1401@naver.com>
2023-11-28 15:41:19 +0900

    :pencil2: Chore: change file name
    chore: change file name

--- front/src/api/feeds/getRecommend.api.ts
+++ front/src/api/feeds/getRecommend.api.ts
@@ -1,6 +1,9 @@
 import { api } from "..";
 
-export const getRecommend = async (postData: { content: string }) => {
+export const getRecommend = async (postData: {
+  content: string;
+  feedId: string;
+}) => {
   const response = await api.post("/recommend/gpt", postData);
   return response;
 };
