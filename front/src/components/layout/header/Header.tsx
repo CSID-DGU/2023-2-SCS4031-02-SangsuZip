c1ce69a1e481a8cf638e628d38f5710ffc29e819
wonho1401 <wonho1401@naver.com>
2023-12-04 03:06:19 +0900

    :sparkles: Feat: add login modal when click login button
    feat: add login modal when click login button

--- front/src/components/layout/header/Header.tsx
+++ front/src/components/layout/header/Header.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
-import Button from "../../common/button/Button";
-import * as S from "./style";
-import theme from "../../../styles/theme";
 import { useNavigate } from "react-router";
+import { useModal } from "@/hooks/useModal";
+import { UserAtom } from "@/stores/UserStore";
+import { useAtomValue } from "jotai";
+import Button from "@/components/common/button/Button";
+import * as S from "./style";
+import theme from "@/styles/theme";
+import ModalWrapper from "@/components/common/modal/ModalWrapper";
+import Login from "@/components/ModalContent/Login/Login";
 
 function Header() {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
+  const nickname = useAtomValue(UserAtom).nickname;
+
+  const { modal, openModal } = useModal();
   const navigate = useNavigate();
 
   const handleScroll = () => {
@@ -30,15 +38,13 @@ function Header() {
       <S.Logo onClick={() => navigate("/")}>Devlog</S.Logo>
       <S.ButtonContainer>
         <Button
-          $width={5}
+          $width={8}
           $height={2}
           $borderColor="white"
           $backgroundColor="white"
           $color={theme.color.point1}
-          text="정원호님"
-          onClick={() => {
-            console.log("zz");
-          }}
+          text={nickname ? `${nickname}님` : ""}
+          onClick={() => {}}
         />
         <Button
           $width={5}
@@ -55,12 +61,11 @@ function Header() {
           $borderColor="white"
           $backgroundColor="white"
           $color={theme.color.point1}
-          text="로그아웃"
-          onClick={() => {
-            console.log("zz");
-          }}
+          text="로그인"
+          onClick={() => openModal("소셜 로그인하기", <Login />)}
         />
       </S.ButtonContainer>
+      {modal.isOpen && <ModalWrapper />}
     </S.Container>
   );
 }
