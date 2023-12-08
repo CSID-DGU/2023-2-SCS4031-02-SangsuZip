94e8fc6e4d755be1661f3aa09157bd91b26b18c8
wonho1401 <wonho1401@naver.com>
2023-12-08 18:40:19 +0900

    :sparkles: Feat: set query client and provider
    feat: set query client and provider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,37 +2,48 @@ import { GlobalStyle } from "./styles/GlobalStyle.tsx";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Provider } from "jotai";
+import { getClient } from "./stores/query/queryClient.ts";
 import Auth from "@/hoc/Auth.tsx";
 import theme from "@/styles/theme.ts";
 import Layout from "@/components/layout/Layout.tsx";
 import Home from "@/pages/Home/Home.tsx";
 import Write from "@/pages/Write/Write.tsx";
 import FeedDetail from "@/pages/FeedDetail/FeedDetail.tsx";
-import { Provider } from "jotai";
+import MyPage from "./pages/MyPage/MyPage.tsx";
 
 function App() {
   const AuthHome = Auth(Home, null);
   const AuthWrite = Auth(Write, true);
   const AuthFeedDetail = Auth(FeedDetail, null);
+  const AuthMyPage = Auth(MyPage, true);
+
+  const queryClient = getClient();
 
   return (
-    <Provider>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <GlobalStyle />
-          <Layout>
-            <Routes>
-              <Route path="/" element={<AuthHome />} />
-              <Route path="/write" element={<AuthWrite />} />
-              <Route
-                path="/feed/:userId/:feedId"
-                element={<AuthFeedDetail />}
-              />
-            </Routes>
-          </Layout>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <GlobalStyle />
+            <Layout>
+              <Routes>
+                <Route path="/" element={<AuthHome />} />
+                <Route path="/write" element={<AuthWrite />} />
+                <Route path="/:userId" element={<AuthMyPage />} />
+                <Route
+                  path="/feed/:userId/:feedId"
+                  element={<AuthFeedDetail />}
+                />
+              </Routes>
+            </Layout>
+            <ReactQueryDevtools />
+          </BrowserRouter>
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
