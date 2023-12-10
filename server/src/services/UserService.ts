3108004e75f08869193e929542942af2dcafa51e
kwan2 <jgh9987@naver.com>
2023-12-11 07:27:50 +0900

    fix: github oauth 설정

--- server/src/services/UserService.ts
+++ server/src/services/UserService.ts
@@ -6,6 +6,7 @@ import axios from 'axios';
 export const checkGithub = async( code : string ) => {
     const url = "https://github.com/login/oauth/access_token";
 
+    console.log(code);
     const body = {
         client_id: process.env.VITE_APP_GITHUB_CLIENT_ID,
         client_secret: process.env.VITE_APP_GITHUB_CLIENT_SECRET,
@@ -16,24 +17,28 @@ export const checkGithub = async( code : string ) => {
         const { data: access_token } = await axios.post(url, body, {
             headers: { Accept: "application/json" },
         });
-
+        
 
         const apiURL = "https://api.github.com";
 
         const { data: userdata } = await axios.get(`${apiURL}/user`, {
-            headers: { Authorization: `token ${access_token}` },
+            headers: { Authorization: `token ${access_token.access_token}` },
         });
 
         const { data: emaildata } = await axios.get(`${apiURL}/user/emails`, {
-            headers: { Authorization: `token ${access_token}` },
+            headers: { Authorization: `token ${access_token.access_token}` },
         });
 
         const username = userdata.login;
         const email = emaildata[0].email;
         const userExists = await User.findOne({ email });
-
+        
         if (userExists) {
-            return userExists;
+            return {
+                userId : userExists._id,
+                email : userExists.email,
+                access_token : userExists.accessToken
+            };
         } else{
             const newUser = new User({
                 username,
